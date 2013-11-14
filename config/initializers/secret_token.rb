c8f127fa9dbd9bf0d39e12b581c70c6859f739e8
Samuel Navas <samuel@batkiwi.com>
2013-11-14 13:43:54 +0100

    initial commit with empty rails4 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleMarkdownBlog::Application.config.secret_key_base = '50b2acb3cb23c79ad381fd634b2fe85c97528965d0fbc09886eb946953512977becba8f45071433556e5434a08a2cec4a5546dce3c6b67af69a3b0da0500cbe4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SimpleMarkdownBlog::Application.config.session_store :cookie_store, key: '_simple-markdown-blog_session'
