0454ab070ac269d2fbfaadd0189a1b08cd4057cc
Samuel Navas <samuel@batkiwi.com>
2013-11-18 19:21:29 +0100

    added some changes to views and added two helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def date datetime
+    "#{datetime.to_date.to_s}"
+  end
+
+  def linkable_title collection
+    collection.each {|item| item.title = link_to item.title, item}
+  end
 end
